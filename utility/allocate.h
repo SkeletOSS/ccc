dacc6ef713cdde95da8b273f97dda34f4911c1de
Alex Lopez <agl.alexglopez@gmail.com>
2026-03-19 07:41:44 -0700

    fix docs

--- utility/allocate.h
+++ utility/allocate.h
@@ -6,16 +6,17 @@
 /** @brief Standard allocator using platform malloc, realloc, and free.
 @param[in] arguments the arguments to the appropriate allocator function.
 @return the result of the operation according to the following
-CCC_Allocator_interface semantics.
+CCC_Allocator_interface semantics which are as follows.
 
 - If input is NULL and bytes 0, NULL is returned.
 - If input is NULL with non-zero bytes, new memory is allocated/returned.
-- If input is non-NULL it has been previously allocated by the
-Allocator_interface.
+- If input is non-NULL it is previously allocated by the Allocator_interface.
 - If input is non-NULL with non-zero size, input is resized to at least bytes
   size. The pointer returned is NULL if resizing fails. Upon success, the
   pointer returned might not be equal to the pointer provided.
-- If input is non-NULL and size is 0, input is freed and NULL is returned. */
+- If input is non-NULL and size is 0, input is freed and NULL is returned.
+
+This allocator does not zero out allocated bytes. */
 void *std_allocate(CCC_Allocator_arguments arguments);
 
 /** @brief A convenience wrapper for the standard library allocator. This can
