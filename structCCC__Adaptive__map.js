57658cd969aacaa466ff7068a2dd22ac11439f93
agl-alexglopez <agl.alexglopez@gmail.com>
2026-04-14 15:03:13 +0000

    Deploying to gh-pages from @ SkeletOSS/ccc@6c5f8b44965782a43aec33cc6eadd5c742d5626f 🚀

--- structCCC__Adaptive__map.js
+++ structCCC__Adaptive__map.js
@@ -1,9 +1,9 @@
 var structCCC__Adaptive__map =
 [
     [ "comparator", "structCCC__Adaptive__map.html#ae445cc9aea21f1d8bdbf44f37d6c77ac", null ],
+    [ "count", "structCCC__Adaptive__map.html#a443255d65bd5daf634c2f7334cedaef5", null ],
     [ "key_offset", "structCCC__Adaptive__map.html#a5f3461b9bfee50ab8786d645548d7610", null ],
     [ "root", "structCCC__Adaptive__map.html#a646f9886c32049cf9a8c5afc8c7c88bf", null ],
-    [ "size", "structCCC__Adaptive__map.html#a5bf6fd16efc35306cc68cbbafcbfc4b8", null ],
     [ "sizeof_type", "structCCC__Adaptive__map.html#aa88f1c9994e525cb16782f55bb7310aa", null ],
     [ "type_intruder_offset", "structCCC__Adaptive__map.html#a2bb70bf3db3d5ace5e205ff4bd1b9816", null ]
 ];
